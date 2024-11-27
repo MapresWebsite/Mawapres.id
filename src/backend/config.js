4e31d934fa17cf063c768979187f2c75b6686378
Kevin <kevinadikasaputragithub@gmail.com>
2024-11-27 16:42:11 +0700

    :construction_worker: Chore: ```config.js``` for connection app to ```.env``` file

---
+++ src/backend/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+module.exports = {
+  database: {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "pbl_prestasi",
+  },
+};
